c84933d80a470497a578e467dbe0d9482eca812c
Sifa Gakeni <gakenisifa@gmail.com>
2025-02-21 12:52:45 +0300

    extended token durations

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=1) 
-app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=6) 
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1) 
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30) 
 app.json.compact = False
 
 metadata = MetaData(naming_convention={
